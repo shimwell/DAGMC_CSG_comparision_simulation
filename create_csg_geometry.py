c49fe9010db18c7a689dc8bab2d40e243cdfc428
shimwell <mail@jshimwell.com>
2022-04-02 22:42:41 +0100

    adding flux tally

--- create_csg_geometry.py
+++ create_csg_geometry.py
@@ -9,37 +9,43 @@ import math
 def run_neutronics_model(
 
 ):
-    ind = input_neutronics_data
-    imd = input_material_data
-    igd = input_geometry_data
 
-    # TODO consider adding units
-    inner_blanket_radius = igd.inner_blanket_radius
-    blanket_thickness = igd.blanket_thickness
-    blanket_height = igd.blanket_height
-    lower_blanket_thickness = igd.lower_blanket_thickness
-    upper_blanket_thickness = igd.upper_blanket_thickness
-    blanket_vv_gap = igd.blanket_vv_gap
-    upper_vv_thickness = igd.upper_vv_thickness
-    vv_thickness = igd.vv_thickness
-    lower_vv_thickness = igd.lower_vv_thickness
+    inner_blanket_radius = 100.
+    blanket_thickness = 70.
+    blanket_height = 500.
+    lower_blanket_thickness = 50.
+    upper_blanket_thickness = 40.
+    blanket_vv_gap = 20.
+    upper_vv_thickness = 10.
+    vv_thickness = 10.
+    lower_vv_thickness = 10.
 
-    simulation_batches = ind.simulation_batches
-    simulation_particles_per_batch = ind.simulation_particles_per_batch
-    fractional_height_of_source = ind.fractional_height_of_source
+    simulation_batches = 10
+    simulation_particles_per_batch = 1000
+    fractional_height_of_source = 0.5
+    
+    blanket_material = 'Li'
+    vessel_material='Li'
 
     mat_blanket = nmm.Material.from_library(
-        name=imd.blanket_material,
-        enrichment=imd.lithium_enrichment,
+        name=blanket_material,
         temperature=400,  # TODO expose temperatue to user args
         temperature_to_neutronics_code=False,
     ).openmc_material
 
-    mat_vessel = nmm.Material.from_library(name=imd.vessel_material, temperature_to_neutronics_code=False).openmc_material
+    mat_vessel = nmm.Material.from_library(
+        name=vessel_material,
+        temperature=400,  # TODO expose temperatue to user args
+        temperature_to_neutronics_code=False
+    ).openmc_material
 
     materials = openmc.Materials([mat_blanket, mat_vessel])
 
-    odd.just_in_time_library_generator(libraries=["ENDFB-7.1-NNDC", "TENDL-2019"], materials=materials, overwrite=False)
+    odd.just_in_time_library_generator(
+        libraries=["ENDFB-7.1-NNDC", "TENDL-2019"],
+        materials=materials,
+        overwrite=False
+    )
 
     # surfaces
     inner_blanket_cylinder = openmc.ZCylinder(r=inner_blanket_radius)
@@ -121,7 +127,7 @@ def run_neutronics_model(
     my_source = openmc.Source()
 
     # sets the location of the source to x=0 y=0 z=0
-    my_source.space = openmc.stats.Point((0, 0, 0))
+    my_source.space = openmc.stats.Point((0, 0, absolute_height_of_source))
 
     # sets the direction to isotropic
     my_source.angle = openmc.stats.Isotropic()
@@ -136,11 +142,9 @@ def run_neutronics_model(
     settings.particles = simulation_particles_per_batch
     settings.source = my_source
     settings.photon_transport = True
-    settings.seed = ind.seed
+    # settings.seed = 1
 
     tallies = openmc.Tallies()
-
-    blanket_cell_filter = openmc.CellFilter([blanket_cell_cylinder, blanket_cell_lower, blanket_cell_upper])
 
     for cell in [
         vessel_cell_lower,
@@ -150,8 +154,9 @@ def run_neutronics_model(
         blanket_cell_upper,
         blanket_cell_lower]:
 
-        flux_tally = openmc.Tally(name=f"flux_{cell}")
-        flux_tally.filters = [cell]
+        flux_tally = openmc.Tally(name=f"flux_{}")
+        cell_filter = openmc.CellFilter([cell])
+        flux_tally.filters = [cell_filter]
         flux_tally.scores = ["flux"]
         tallies.append(flux_tally)
 
@@ -161,8 +166,16 @@ def run_neutronics_model(
 
     sp = openmc.StatePoint(sp_filename)
 
-    for tally in sp.tallies:
-        print(tally)
+    tbr_tally = sp.get_tally(name='TBR')
+    df = tbr_tally.get_pandas_dataframe()
+
+    tbr_tally_result = df['mean'].sum()
+    tbr_tally_std_dev = df['std. dev.'].sum()
+
+    # for key, value in sp.tallies.items:
+    #     print(tally.mean())
+    # print(sp.tallies)
+    # print(tally.__dict__)
     # df = tbr_tally.get_pandas_dataframe()
     # tbr_tally_result = df["mean"].sum()
 
@@ -178,11 +191,13 @@ def run_neutronics_model(
     # return output_data
 
 
-def volume_of_hollow_cylinder(
-    outer_radius: float,
-    height: float,
-    inner_radius: float = 0,
-):
-    # = π (R2 - r2) h
-    vol = math.pi * (math.pow(outer_radius, 2) - math.pow(inner_radius, 2)) * height
-    return vol
+# def volume_of_hollow_cylinder(
+#     outer_radius: float,
+#     height: float,
+#     inner_radius: float = 0,
+# ):
+#     # = π (R2 - r2) h
+#     vol = math.pi * (math.pow(outer_radius, 2) - math.pow(inner_radius, 2)) * height
+#     return vol
+
+run_neutronics_model()
